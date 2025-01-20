d396155500171060c0fe6eb9a86033dbbc1c9ed8
H0RIS@outlook.com <H0RIS@outlook.com>
2025-01-20 22:40:54 +0100

    TRYTHIS 2

--- Chapter2/Exercise_01/Main.cpp
+++ Chapter2/Exercise_01/Main.cpp
@@ -6,6 +6,14 @@
  Read the age into a double to allow for children who can be very proud of being five
  and a half years old rather than just five.
 
+ 2.
+ Get this little program to run. Then, modify it to read an int rather than a double.
+ Also, ‘‘exercise’’ some other operations, such as the modulo operator, %. Note that
+ for ints / is integer division and % is remainder (modulo), so that 5/2 is 2 (and not 2.5
+ or 3) and 5%2 is 1. The definitions of integer ∗, /, and % guarantee that for two positive
+ ints a and b we have a/b ∗ b + a%b == a.
+
+ 3.
 
 */
 
@@ -27,8 +35,22 @@ int main()
         std::cin >> first_name >> age;
         std::cout << "Hello, " << first_name << "(age " << age * 12.0 << " months)\n";
     }
-        break;
-    case 2: std::cout << "You entered two." << std::endl;
+    break;
+    case 2:
+    {
+        std::cout << "Please enter an int value: ";
+        int n{ 0 };
+        std::cin >> n;
+        std::cout << "n == " << n
+            << "\nn+1 == " << n + 1
+            << "\nthree times n == " << n * 3
+            << "\ntwice n == " << n * 2
+            << "\nn squared == " << n * n
+            << "\nhalf of n == " << n / 2
+            << "\nsquare root of n == " << std::sqrt(n)
+            << "\nn mod 2 == " << n % 2
+            << '\n';
+    }
         break;
     case 3: std::cout << "You entered three." << std::endl;
         break;
