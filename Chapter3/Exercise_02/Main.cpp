37c5a5dd8d4bd3cb881c5e8b40d0e49397743464
H0RIS@outlook.com <H0RIS@outlook.com>
2025-02-10 13:51:49 +0100

    E2

--- Chapter3/Exercise_02/Main.cpp
+++ Chapter3/Exercise_02/Main.cpp
@@ -1,8 +1,19 @@
+/*
+Write a program that reads a string from input and then, for each character read, prints out
+ the character and its integer value on a line.
+*/
+
 import std;
 
 int main()
 {
-    std::cout << "Hello, World!\n";
+    std::cout << "Enter a string: ";
+    std::string string{};
+    std::cin >> string;
 
+    for (size_t i = 0; i < string.size(); i++)
+    {
+        std::cout << string[i] << '\t' << static_cast<int>(string[i]) << '\n';
+    }
     return 0;
 }
